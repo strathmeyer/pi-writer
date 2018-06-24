030eeab0ac606f07ff4e9e65c7fefe8a74447383
Eric Strathmeyer <eric@vawks.com>
2018-06-23 23:39:43 -0700

    Use curses to log every key

--- pi-writer.py
+++ pi-writer.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import curses
+import curses.ascii
 import os
 import sys
 
@@ -20,24 +22,60 @@ def latest_file():
     return None
 
 
-def main():
-  filename = latest_file() or new_file()
+def desired_char(char):
+  return curses.ascii.isprint(char) or \
+    char == '\n'
+
+
+def get_and_write(file, stdscr):
+  cursor_pos = 0
+  stdscr.move(1, 0)
 
   while True:
-    path = os.path.join(FILE_STORE, filename)
-    print("Writing to:", filename)
+    chr_code = stdscr.getch(1, cursor_pos)
 
-    with open(path, 'a') as f:
-      while True:
-        try:
-          line = input()
-        except EOFError:
-          break
-        f.write(line + '\n')
-        f.flush()
+    try:
+      char = chr(chr_code)
+    except ValueError:
+      continue
 
-    filename = new_file()
+    if char == curses.ascii.ctrl('d'):
+      stdscr.clear()
+      return
+    elif char == '\n':
+      file.write(char)
+      file.flush()
+      cursor_pos = 0
+      stdscr.move(1, 0)
+      stdscr.clrtoeol()
+    elif desired_char(char):
+      stdscr.addstr(1, cursor_pos, char)
+      file.write(char)
+      file.flush()
+      (_, width) = stdscr.getmaxyx()
+      cursor_pos = min(cursor_pos + 1, width - 1)
+
+    stdscr.refresh()
+
+
+def main(stdscr):
+  curses.curs_set(0)
+
+  filename = latest_file() or new_file()
+
+  try:
+    while True:
+      path = os.path.join(FILE_STORE, filename)
+      stdscr.addstr(0, 0, "Writing to: " + filename)
+
+      with open(path, 'a') as f:
+        get_and_write(f, stdscr)
+
+      filename = new_file()
+
+  except KeyboardInterrupt:
+    sys.exit(0)
 
 
 if __name__ == '__main__':
-  main()
+  curses.wrapper(main)
